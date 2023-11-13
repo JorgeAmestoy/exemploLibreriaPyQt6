8f0fe352986d2be681ec03c9dff1de3406cef50e
jorgeamestoy <jamestoysabucedo@danielcastelao.org>
2023-11-13 10:20:16 +0100

    Actualizamos ejemploCheckBoxRadio.py

--- ejemploCheckBoxRadio.py
+++ ejemploCheckBoxRadio.py
@@ -1,54 +1,107 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QCheckBox
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QCheckBox, QRadioButton
 
-class ejemploCheckBoxRadio(QMainWindow):#clase que hereda las propiedades de QMainWindowss
-    def __init__(self):#Lo primero que hacemos es llamar al inicializador(constructor) de la super clase
+
+class VentanaPrincipal (QMainWindow):
+    def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Ejemplo con QCheckBox")
+        self.setWindowTitle( "Ejemplo con QCheckBox y QRadioButton")
 
-        cajaVertical = QVBoxLayout()
+        cajaV1 = QVBoxLayout()
 
-        #Al poner varios funciones, pongo el self antes de los checkBotones para diferenciarlos¿?
-        self.checkBoton1 = QCheckBox("Boton 1")
-        self.checkBoton1.toggled.connect(self.on_checkBoton1_toggled)
-        cajaVertical.addWidget(self.checkBoton1)
+        self.chkBoton1 = QCheckBox ("Boton 1")
+        self.chkBoton1.toggled.connect(self.on_chkBoton1_toggled)
+        cajaV1.addWidget(self.chkBoton1)
 
-        self.checkBoton2 = QCheckBox("Boton 2")
-        self.checkBoton2.toggled.connect(self.on_checkBoton2_toggled)
-        cajaVertical.addWidget(self.checkBoton2)
+        self.chkBoton2 = QCheckBox ("Boton 2")
+        self.chkBoton2.toggled.connect (self.on_chkBoton2_toggled)
+        cajaV1.addWidget(self.chkBoton2)
+
+        #Metemos el layout cajaVertical2 en el wdiget llamado containerV2 y lo añadimos al layout de la caja vertical 1
+        cajaV2 = QVBoxLayout()
+        containerV2 = QWidget()
+        containerV2.setLayout(cajaV2)
+        cajaV1.addWidget(containerV2)#Aqui añadimos widget en vez de layout
+
+        self.rbtRadioButton1 = QRadioButton("Opción 1", containerV2)
+        self.rbtRadioButton1.toggled.connect (self.on_rbtRadioButton1_toggled)
+        cajaV2.addWidget(self.rbtRadioButton1)
+
+        self.rbtRadioButton2 = QRadioButton("Opción 2", containerV2)
+        self.rbtRadioButton2.toggled.connect(self.on_rbtRadioButton2_toggled)
+        cajaV2.addWidget(self.rbtRadioButton2)
+
+        self.rbtRadioButton3 = QRadioButton("Opción 3", containerV2)
+        self.rbtRadioButton3.toggled.connect(self.on_rbtRadioButton3_toggled)
+        cajaV2.addWidget(self.rbtRadioButton3)
+
+        cajaV3 = QVBoxLayout()
+        cajaV1.addLayout(cajaV3)
+
+        self.rbtRadioButton4 = QRadioButton("Opción 1 Grupo 2")
+        self.rbtRadioButton4.toggled.connect(self.on_rbtRadioButton1_toggled)
+        cajaV3.addWidget(self.rbtRadioButton4)
+
+        self.rbtRadioButton5 = QRadioButton("Opción 2 Grupo 2")
+        self.rbtRadioButton5.toggled.connect(self.on_rbtRadioButton2_toggled)
+        cajaV3.addWidget(self.rbtRadioButton5)
+
+        self.rbtRadioButton6 = QRadioButton("Opción 3 Grupo 2")
+        self.rbtRadioButton6.toggled.connect(self.on_rbtRadioButton3_toggled)
+        cajaV3.addWidget(self.rbtRadioButton6)
+
+
 
 
         container = QWidget()
-        container.setLayout(cajaVertical)
+        container.setLayout(cajaV1)
         self.setCentralWidget(container)
 
         self.setFixedSize(400,300)
         self.show()
 
-    def on_checkBoton1_toggled(self):
-        if self.checkBoton1.isChecked():
-            print("Boton check seleccionado: ", self.checkBoton1.text())
+    def on_btnSaludo_clicked(self):
+        saludo = self.txtSaludo.text()
+        self.lblEtiqueta1.setText(saludo)
+
+
+    def on_chkBoton1_toggled(self):
+        if self.chkBoton1.isChecked():
+            print ("Boton check seleccionado: ", self.chkBoton1.text())
         else:
-            print("Boton check deseleccionado ", self.checkBoton1.text())
+            print("Boton check deseleccionado: ", self.chkBoton1.text())
 
-    def on_checkBoton2_toggled(self):
-        if self.checkBoton2.isChecked():
-            print("Boton check seleccionado: ",self.checkBoton2.text())
+    def on_chkBoton2_toggled(self):
+        if self.chkBoton2.isChecked():
+            print ("boton check seleccionado: ", self.chkBoton2.text())
         else:
-            print("Boton check deseleccionado ",self.checkBoton2.text())
+            print("Boton check deseleccionado: ", self.chkBoton2.text())
 
 
+    def on_rbtRadioButton1_toggled(self):
+        if self.rbtRadioButton1.isChecked():
+            print ("Boton check seleccionado: ", self.rbtRadioButton1.text())
+        else:
+            print("Boton check deseleccionado: ", self.rbtRadioButton1.text())
 
-    def on_botonSaludo_clicked(self):#Tiene que llamarse igual que como lo declaraste en el clicked.connect.
-        self.lblEtiqueta1.setText("Hola companheros!!!")
+
+    def on_rbtRadioButton2_toggled(self):
+        if self.rbtRadioButton2.isChecked():
+            print ("Boton check seleccionado: ", self.rbtRadioButton2.text())
+        else:
+            print("Boton check deseleccionado: ", self.rbtRadioButton2.text())
 
 
+    def on_rbtRadioButton3_toggled(self):
+        if self.rbtRadioButton3.isChecked():
+            print ("Boton check seleccionado: ", self.rbtRadioButton3.text())
+        else:
+            print("Boton check deseleccionado: ", self.rbtRadioButton3.text())
 
-
-if __name__ =="__main__":
+if __name__=="__main__":
     aplicacion = QApplication(sys.argv)
-    ventana = ejemploCheckBoxRadio()#creamos instancia de nuestra primera ventana. Como en JAVA, llamamos a la clase.
+    ventana = VentanaPrincipal()
     aplicacion.exec()
