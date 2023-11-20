14113cd33c6e1cd5f500d7ea5c3526d5c80c2428
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-11-21 00:48:32 +0100

    Creo nuevo módulo lantilla.py y EjerciciosCasa.py para probar

---
+++ plantilla.py
@@ -0,0 +1,29 @@
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import sys
+
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Mi primera aplicacion")
+
+
+
+
+
+
+        self.setFixedSize(400, 800)
+
+
+
+aplicacion = QApplication(sys.argv)
+ventana = VentanaPrincipal()
+ventana.show()
+aplicacion.exec()
