63d4649f48cc6f55f79b4f9cecbbe0c9ae50c29c
jorgeamestoy <jamestoysabucedo@danielcastelao.org>
2023-11-21 10:23:51 +0100

    Añado ejemploQListView.py

--- ejemploQListView.py
+++ ejemploQListView.py
@@ -5,10 +5,11 @@ import typing
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QVBoxLayout,
                             QWidget, QListView, QHBoxLayout, QLineEdit)
 from PyQt6.QtCore import Qt, QAbstractListModel
+from PyQt6.QtGui import QImage
 
 
 
-
+tickImage = QImage('cheque.png')
 # Clase TareasModelo que hereda propiedades de QAbstractListModel. Hereda funcionalidades para trabajar con una lista
 class TareasModelo(QAbstractListModel):
 
@@ -23,14 +24,20 @@ class TareasModelo(QAbstractListModel):
    def data(self, indice, rol):
 
        if (rol == Qt.ItemDataRole.DisplayRole):# En caso de que el rol sea el de visualización del ítem, en este caso, un texto visible...:
-           estado, texto = self.tareas[indice.row()] # Accede al elemento de la lista (self.tareas) correspondiente a la posición indicada por el índice de fila (indice.row()). Se asume que cada elemento es una tupla con al menos dos elementos: un estado y un texto. Recogemos una fila y nos da el estado y el texto, y solo nos interesa el texto. ASi data nos devuelve el texto que metamos en tarefas.
+           _, texto = self.tareas[indice.row()] # Accede al elemento de la lista (self.tareas) correspondiente a la posición indicada por el índice de fila (indice.row()). Se asume que cada elemento es una tupla con al menos dos elementos: un estado y un texto. Recogemos una fila y nos da el estado y el texto, y solo nos interesa el texto. ASi data nos devuelve el texto que metamos en tarefas.
            return texto # Devuelve el texto de la tarea
+
+       if (rol ==Qt.ItemDataRole.DecorationRole): # Verifica si es una imagen: En caso de que lo sea..:
+           estado,_ = self.tareas[indice.row()] # EL guion significa que no le ponemos nombre a la variable. Aqui solo pondremos el estado aunque trabajemos en una tupla de dos. LO mismo en el de arriba.
+           #Barrabaja es porque decimos que vamos a trabajar con una tupla de dos elementos pero que solo vamos a trabajar con uno, en este caso, el estado.
+           if estado:
+               return tickImage
 
 
    # Método que también heredo de la clase Padre QAbstractListModel que me da el número total de filas(items)
    def rowCount(self, indice):
        return len(self.tareas) # Devuelve la cantidad de tareas en el modelo. La definicion del metodo pone que necesita un indice por eso lo ponemos obloigaotirmanete, aunque luego no lo usemos como se puede ver. Poniendo curso encima de rowCount aparecen los parametros obligatorios.
-
+    # Metodo interno. Para que el modelo sepa con qué trabaja y cómo saber cuantas filas tiene.
 
 class VentanaPrincipal(QMainWindow):
    # Constructor de la clase e inicio clase Padre y esta misma
@@ -55,6 +62,7 @@ class VentanaPrincipal(QMainWindow):
        # Configuración de la vista de lista y asignación del modelo
        self.lstTareas = QListView()# LE PONGO EL SELF PORQUE LO USO EN EL METODO DE ABAJO DE BTNBORRAR
        self.lstTareas.setModel(self.modelo)# Añado el modelo (instancia de la clase TareasModelo) al QListView
+       self.lstTareas.setSelectionMode(QListView.SelectionMode.MultiSelection) # Para poder seleccionar varias tareas (items). Hay que seleccionar en orden de abarjo para arriba.
        cajaV.addWidget(self.lstTareas)# Añado el QListView al layout vertical
 
 
@@ -63,6 +71,7 @@ class VentanaPrincipal(QMainWindow):
        btnBorrar = QPushButton("Borrar")
        btnBorrar.pressed.connect(self.on_btnBorrar_pressed)
        btnHecho = QPushButton("Hecho")
+       btnHecho.pressed.connect(self.on_btnHecho_pressed)
        cajaH.addWidget(btnBorrar)
        cajaH.addWidget(btnHecho)
 
@@ -102,14 +111,28 @@ class VentanaPrincipal(QMainWindow):
 
 
    def on_btnBorrar_pressed(self):
-       indices = self.lstTareas.selectedIndexes()# COnseguimos a lista de los elementos seleccionados. Nos da uno o varios indices.
-       if indices:
-           for indice in indices: # Recorremos los indices en caso de haber encontrado varios.
-               del self.modelo.tareas[indice.row()]# Borramos
-           self.modelo.layoutChanged.emit()# Para actualizar la vista
+       indices = self.lstTareas.selectedIndexes()# Obtenemos el/los indice(s) del elemento(s) seleccionado(s)
+       if indices: # Si encuentra un indice..:
+           for indice in sorted(indices, reverse=True): # Recorremos los indices en caso de haber encontrado varios. Usamos el reverse para poder hacerlo bien sino da error.
+                del self.modelo.tareas[indice.row()]# Borramos según va recorriendo
+           self.modelo.layoutChanged.emit()# Para actualizar la vista. Se utiliza para notificar a la vista sobre cambios estructurales que afectan a la presentación general de los datos.
            self.lstTareas.clearSelection() # Para cuando marques tarea que quieres eliminar, no quede seleccionado.
 
 
+   def on_btnHecho_pressed(self):
+       indices = self.lstTareas.selectedIndexes()# Obtenemos los indices de los elementos seleccionados
+       if indices:
+           for indice in indices:
+               estado,texto = self.modelo.tareas[indice.row()]# Recogemos los elemenetos
+               self.modelo.tareas[indice.row()] = (True, texto) # Recogiendolos le cambios el estado a True
+              # self.modelo.tareas[indice.row()] = (True, self.modelo.tareas[indice.row()][1]). Otra forma de escribir las dos lineas de arriba
+           self.modelo.dataChanged.emit(indice, indice) #  Se utiliza para notificar a la vista sobre cambios específicos en los datos dentro del modelo.
+           self.lstTareas.clearSelection() # Para cuando marques tarea que quieres eliminar, no quede seleccionado.
+
+
+
+
+#DIFERENCIA ENTRE PONER EL METODO () O SIN ()?
 if __name__ == "__main__":
    aplicacion = QApplication(sys.argv)
    ventana = VentanaPrincipal()
